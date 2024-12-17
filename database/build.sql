ff7d4911d55f4b048a612e936e9085491a0a1256
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2024-12-17 14:19:15 +0100

    KAN-41 Add create table for params, sensors, measurements

--- database/build.sql
+++ database/build.sql
@@ -9,7 +9,7 @@ CREATE TABLE districts
     id          SERIAL PRIMARY KEY,
     name        VARCHAR(100) UNIQUE NOT NULL,
     province_id INT                 NOT NULL,
-    FOREIGN KEY (province_id) REFERENCES provinces (id)
+    FOREIGN KEY (province_id) REFERENCES provinces (id) ON DELETE CASCADE
 );
 
 CREATE TABLE communes
@@ -17,7 +17,7 @@ CREATE TABLE communes
     id          SERIAL PRIMARY KEY,
     name        VARCHAR(100) UNIQUE NOT NULL,
     district_id INT                 NOT NULL,
-    FOREIGN KEY (district_id) REFERENCES districts (id)
+    FOREIGN KEY (district_id) REFERENCES districts (id) ON DELETE CASCADE
 );
 
 CREATE TABLE cities
@@ -25,7 +25,7 @@ CREATE TABLE cities
     id         SERIAL PRIMARY KEY,
     name       VARCHAR(100) UNIQUE NOT NULL,
     commune_id INT                 NOT NULL,
-    FOREIGN KEY (commune_id) REFERENCES communes (id)
+    FOREIGN KEY (commune_id) REFERENCES communes (id) ON DELETE CASCADE
 );
 
 CREATE TABLE addresses
@@ -33,7 +33,7 @@ CREATE TABLE addresses
     id      SERIAL PRIMARY KEY,
     name    VARCHAR(255),
     city_id INT,
-    FOREIGN KEY (city_id) REFERENCES cities (id)
+    FOREIGN KEY (city_id) REFERENCES cities (id) ON DELETE CASCADE
 );
 
 CREATE TABLE stations
@@ -43,5 +43,34 @@ CREATE TABLE stations
     latitude     DOUBLE PRECISION NOT NULL,
     longitude    DOUBLE PRECISION NOT NULL,
     address_id   INT,
-    FOREIGN KEY (address_id) REFERENCES addresses (id)
+    FOREIGN KEY (address_id) REFERENCES addresses (id) ON DELETE CASCADE
 );
+
+CREATE TABLE params
+(
+    id      SERIAL PRIMARY KEY,
+    name    VARCHAR(255)       NOT NULL,
+    formula VARCHAR(50)        NOT NULL,
+    code    VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE sensors
+(
+    id         SERIAL PRIMARY KEY,
+    station_id INT,
+    param_id   INT,
+    FOREIGN KEY (station_id) REFERENCES stations (id) ON DELETE CASCADE,
+    FOREIGN KEY (param_id) REFERENCES params (id) ON DELETE CASCADE
+);
+
+CREATE TABLE measurements
+(
+    id         SERIAL PRIMARY KEY,
+    date       TIMESTAMP,
+    value      DOUBLE PRECISION NOT NULL,
+    param_code VARCHAR(50)      NOT NULL,
+    sensor_id  INT,
+    FOREIGN KEY (param_code) REFERENCES params (code) ON DELETE CASCADE,
+    FOREIGN KEY (sensor_id) REFERENCES sensors (id) ON DELETE CASCADE
+);
+
