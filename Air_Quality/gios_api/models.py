a36609cdff83d24a29923819f010f86d05f5e637
krzy-sok <01178595@pw.edu.pl>
2025-01-11 20:15:15 +0100

    added location models

--- Air_Quality/gios_api/models.py
+++ Air_Quality/gios_api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Province(models.Model):
+    name = models.CharField(max_length=100)
+
+class District(models.Model):
+    name = models.CharField(max_length=100)
+    commune_id = models.ForeignKey(Province)
+
+class Commune(models.Model):
+    name = models.CharField(max_length=100)
+    commune_id = models.ForeignKey(District)
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    commune_id = models.ForeignKey(Commune)
+
+class Address(models.Model):
+    name = models.CharField(max_length=255)
+    city_id = models.ForeignKey(City)
+
+class Station(models.Model):
+    station_name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    address_id = models.ForeignKey(Address)
