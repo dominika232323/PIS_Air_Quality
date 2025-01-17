69b4dbc0dbefc00c9af3a93f87cdc92107e0dc3c
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-01-17 20:07:50 +0100

    KAN-45 Add urls for viewsets

--- Air_Quality/gios_api/urls.py
+++ Air_Quality/gios_api/urls.py
@@ -1,19 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from views import get_station_info, get_all_stations, streamlit_app
-from .views.address_view import AddressViewSet
-from .views.air_quality_level_view import AirQualityLevelViewSet
-from .views.air_quality_pollutant_view import AirQualityPollutantViewSet
-from .views.air_quality_view import AirQualityViewSet
-from .views.city_view import CityViewSet
-from .views.commune_view import CommuneViewSet
-from .views.district_view import DistrictViewSet
-from .views.measurement_view import MeasurementViewSet
-from .views.parameter_view import ParameterViewSet
-from .views.province_view import ProvinceViewSet
-from .views.sensor_view import SensorViewSet
-from .views.station_view import StationViewSet
+from gios_api import views
+from .view_sets.address_view import AddressViewSet
+from .view_sets.air_quality_level_view import AirQualityLevelViewSet
+from .view_sets.air_quality_pollutant_view import AirQualityPollutantViewSet
+from .view_sets.air_quality_view import AirQualityViewSet
+from .view_sets.city_view import CityViewSet
+from .view_sets.commune_view import CommuneViewSet
+from .view_sets.district_view import DistrictViewSet
+from .view_sets.measurement_view import MeasurementViewSet
+from .view_sets.parameter_view import ParameterViewSet
+from .view_sets.province_view import ProvinceViewSet
+from .view_sets.sensor_view import SensorViewSet
+from .view_sets.station_view import StationViewSet
 
 
 router = DefaultRouter()
@@ -33,7 +33,7 @@ router.register(r"stations", StationViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     #path('', views.welcome_user, name='welcome_page'),
-    path('<int:station_id>/', get_station_info, name='station_info'),
-    path('all/', get_all_stations, name='all_stations'),
-    path('', streamlit_app, name='all_stations')
+    path('<int:station_id>/', views.get_station_info, name='station_info'),
+    path('all/', views.get_all_stations, name='all_stations'),
+    path('', views.streamlit_app, name='all_stations')
 ]
