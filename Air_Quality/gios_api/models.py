ec643e58046d105893f5f99eb29c4e2fa6f04309
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-01-17 20:05:51 +0100

    KAN-45 Add class Meta to models

--- Air_Quality/gios_api/models.py
+++ Air_Quality/gios_api/models.py
@@ -8,6 +8,10 @@ class Province(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        managed = False
+        db_table = "provinces"
+
 
 class District(models.Model):
     name = models.CharField(max_length=100)
@@ -15,6 +19,10 @@ class District(models.Model):
 
     def __str__(self):
         return f"{self.name} in {self.province.name}"
+
+    class Meta:
+        managed = False
+        db_table = "districts"
 
 
 class Commune(models.Model):
@@ -24,6 +32,10 @@ class Commune(models.Model):
     def __str__(self):
         return f"{self.name} in {self.district.name}"
 
+    class Meta:
+        managed = False
+        db_table = "communes"
+
 
 class City(models.Model):
     name = models.CharField(max_length=100)
@@ -32,6 +44,10 @@ class City(models.Model):
     def __str__(self):
         return f"{self.name} in {self.commune.name}"
 
+    class Meta:
+        managed = False
+        db_table = "cities"
+
 
 class Address(models.Model):
     name = models.CharField(max_length=255)
@@ -39,6 +55,10 @@ class Address(models.Model):
 
     def __str__(self):
         return f"{self.name} in {self.city.name}"
+
+    class Meta:
+        managed = False
+        db_table = "addresses"
 
 
 class Station(models.Model):
@@ -50,6 +70,10 @@ class Station(models.Model):
     def __str__(self):
         return f"{self.station_name} at ({self.latitude}, {self.longitude}) in {self.address.name}"
 
+    class Meta:
+        managed = False
+        db_table = "stations"
+
 
 class Parameter(models.Model):
     name = models.CharField(max_length=255)
@@ -59,6 +83,10 @@ class Parameter(models.Model):
     def __str__(self):
         return f"{self.name} (Formula: {self.formula}, Code: {self.code})"
 
+    class Meta:
+        managed = False
+        db_table = "params"
+
 
 class Sensor(models.Model):
     station = models.ForeignKey(Station, null=True, on_delete=models.SET_NULL)
@@ -66,6 +94,10 @@ class Sensor(models.Model):
 
     def __str__(self):
         return f"Sensor at {self.station.station_name} measuring {self.parameter.name}"
+
+    class Meta:
+        managed = False
+        db_table = "sensors"
 
 
 class Measurement(models.Model):
@@ -77,12 +109,20 @@ class Measurement(models.Model):
     def __str__(self):
         return f"{self.value} for {self.param_code.name} on {self.date}"
 
+    class Meta:
+        managed = False
+        db_table = "measurements"
+
 
 class AirQualityLevel(models.Model):
     level_name = models.CharField(max_length=50)
 
     def __str__(self):
         return f"{self.level_name}"
+
+    class Meta:
+        managed = False
+        db_table = "air_quality_levels"
 
 
 class AirQuality(models.Model):
@@ -96,6 +136,10 @@ class AirQuality(models.Model):
     def __str__(self):
         return f"{self.quality_level.level_name} at {self.station} on {self.calculate_date}: "
 
+    class Meta:
+        managed = False
+        db_table = "air_quality"
+
 
 class AirQualityPollutant(models.Model):
     air_quality = models.ForeignKey(AirQuality, null=True, on_delete=models.SET_NULL)
@@ -106,3 +150,7 @@ class AirQualityPollutant(models.Model):
 
     def __str__(self):
         return f"{self.parameter} for {self.air_quality.station.station_name} at {self.calculate_date} "
+
+    class Meta:
+        managed = False
+        db_table = "air_quality_pollutants"
