d2d38380a7436f65fde273b99ae5d474913138a6
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-01-17 17:49:51 +0100

    KAN-45 Add __str__ for models

--- Air_Quality/gios_api/models.py
+++ Air_Quality/gios_api/models.py
@@ -5,21 +5,41 @@ from django.db import models
 class Province(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+
 class District(models.Model):
     name = models.CharField(max_length=100)
     province = models.ForeignKey(Province, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.name} in {self.province.name}"
+
 
 class Commune(models.Model):
     name = models.CharField(max_length=100)
     district = models.ForeignKey(District, null=True, on_delete=models.SET_NULL)
 
+    def __str__(self):
+        return f"{self.name} in {self.district.name}"
+
+
 class City(models.Model):
     name = models.CharField(max_length=100)
     commune = models.ForeignKey(Commune, null=True, on_delete=models.SET_NULL)
 
+    def __str__(self):
+        return f"{self.name} in {self.commune.name}"
+
+
 class Address(models.Model):
     name = models.CharField(max_length=255)
     city = models.ForeignKey(City, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.name} in {self.city.name}"
+
 
 class Station(models.Model):
     station_name = models.CharField(max_length=255)
@@ -27,14 +47,26 @@ class Station(models.Model):
     longitude = models.FloatField()
     address = models.ForeignKey(Address, null=True, on_delete=models.SET_NULL)
 
+    def __str__(self):
+        return f"{self.station_name} at ({self.latitude}, {self.longitude}) in {self.address.name}"
+
+
 class Parameter(models.Model):
     name = models.CharField(max_length=255)
     formula = models.CharField(max_length=50)
     code = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.name} (Formula: {self.formula}, Code: {self.code})"
+
+
 class Sensor(models.Model):
     station = models.ForeignKey(Station, null=True, on_delete=models.SET_NULL)
     parameter = models.ForeignKey(Parameter, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"Sensor at {self.station.station_name} measuring {self.parameter.name}"
+
 
 class Measurement(models.Model):
     date = models.DateField()
@@ -42,8 +74,16 @@ class Measurement(models.Model):
     param_code = models.ForeignKey(Parameter, null=True, on_delete=models.SET_NULL)
     sensor = models.ForeignKey(Sensor, null=True, on_delete=models.SET_NULL)
 
+    def __str__(self):
+        return f"{self.value} for {self.param_code.name} on {self.date}"
+
+
 class AirQualityLevel(models.Model):
     level_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.level_name}"
+
 
 class AirQuality(models.Model):
     station = models.ForeignKey(Station, null=True, on_delete=models.SET_NULL)
@@ -53,9 +93,16 @@ class AirQuality(models.Model):
     index_status = models.BooleanField()
     critical_param = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.quality_level.level_name} at {self.station} on {self.calculate_date}: "
+
+
 class AirQualityPollutant(models.Model):
     air_quality = models.ForeignKey(AirQuality, null=True, on_delete=models.SET_NULL)
     parameter = models.ForeignKey(Parameter ,null=True, on_delete=models.SET_NULL)
     calculate_date = models.DateField()
     quality_level = models.ForeignKey(AirQualityLevel ,null=True, on_delete=models.SET_NULL)
     source_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.parameter} for {self.air_quality.station.station_name} at {self.calculate_date} "
